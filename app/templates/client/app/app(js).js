25d9e008cb4ffa82ffb66641184266b1a92d3aa7
Mark Chapman <mark.chapman@gmail.com>
2014-07-23 00:55:22 +0100

    Explicitly add routing prefix

--- app/templates/client/app/app(js).js
+++ app/templates/client/app/app(js).js
@@ -52,6 +52,6 @@ angular.module('<%= scriptAppName %>', [<%= angularModules %>])
   })<% } %>;
 
   formsAngular.config(['cssFrameworkServiceProvider', 'routingServiceProvider', function (cssFrameworkService, routingService) {
-      routingService.start({html5Mode: true, routing: '<% if(filters.ngroute) { %>ngroute<% } %><% if(filters.uirouter) { %>uirouter<% } %>'});
+      routingService.start({html5Mode: true, prefix:'/data', routing: '<% if(filters.ngroute) { %>ngroute<% } %><% if(filters.uirouter) { %>uirouter<% } %>'});
       cssFrameworkService.setOptions({framework: '<% if(filters.bs3) { %>bs3<% } %><% if(filters.bs2) { %>bs2<% } %>'});
       }]);
