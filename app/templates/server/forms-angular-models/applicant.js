a3a1a349677ff0cde1452a72e34d2e0703dbb565
Mark Chapman <mark.chapman@gmail.com>
2015-10-21 00:41:49 +0100

    chore(depends) Update to new(ish) fngJqUpload API

--- app/templates/server/forms-angular-models/applicant.js
+++ app/templates/server/forms-angular-models/applicant.js
@@ -2,18 +2,13 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
-<% if(filters.jqUpload) { %>
-var uploadSchema = new mongoose.Schema({
-  filename: String,
-  size: Number
-});<% } %>
+<% if(filters.jqUpload) { %>var jqUploads = require('fng-jq-upload');<% } %>
 
 var ApplicantSchema = new Schema({
   surname: {type:String, required:true, index:true},
   forename: {type:String, index:true}<% if(filters.uiDate) { %>,
   dateOfBirth: {type:Date}<% } %><% if(filters.jqUpload) { %>,
-  photo: {type: [uploadSchema], form: {directive: 'fng-jq-upload-form', add:{autoUpload:true, sizeLimit:50000000}}}<% } %><% if(filters.ckeditor) { %>,
+  photo: {type: [new Schema(jqUploads.FileSchema)], form: { true, directive: 'fng-jq-upload-form', fngJqUploadForm:{sizeLimit:524288, autoUpload:true, single:true, width: 100, height: 100 }}},<% } %><% if(filters.ckeditor) { %>,
   whyApplied: {type: String, form: {type: 'textarea', editor: 'ckEditor'}}<% } %>,
   status: {type: String, default:'Pending', enum:['Pending','Rejected','Shortlist']<% if(filters.select2) { %>, form: {select2: {}}<% } %><% if(filters.uiSelect) { %>, form: {size: 'small', directive: 'fng-ui-select', fngUiSelect:{theme: "bootstrap"}}<% } %>}
 });
